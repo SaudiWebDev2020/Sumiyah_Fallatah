9d53d88903894a305870a090299a410438ec183b
Sumiyah <sumiyah.fa@gmail.com>
2021-01-03 11:03:35 +0300

    First Java Program

---
+++ JavaAssignments/FirstJava/firstJava.java
@@ -0,0 +1,7 @@
+public class firstJava{
+    public static void main(String[] args) {
+        System.out.println("My name is Sumiyah");
+        System.out.println("I am 99 years old");
+        System.out.println("My hometown is Yanbu, Al Madinah");
+    }
+}
